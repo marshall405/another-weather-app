bfe55fdd2631b498037229e72ff0065e191930a4
Marshall Slemp <marshall.slemp@gmail.com>
2018-10-06 23:41:50 -0500

    add styles and more features for weather

--- src/ShowTemp.js
+++ src/ShowTemp.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
+import {colorStyles} from './styles/facebookStyles';
+
+const divStyles = {
+    color: colorStyles.darkBlue,
+
+}
+
+
+
 export const ShowTemp = (props) => {
     if(!props.error){
         return (
-            <div>
+            <div style={divStyles}>
                 <h1> Weather </h1>
                 <h2> {props.city} </h2>
+                <h4> {props.sky} </h4>
                 <h3> Temp {props.temp}&#8457; </h3>
+                <div style={{display:'flex', justifyContent:'space-around', alignItems:'center' }}>
+                    <p> Sunrise {props.sunrise} </p>
+                    <p> Sunset {props.sunset} </p>
+                </div>
             </div>
         )
     } else {
         return (
             <div>
                 <h1> Weather </h1>
-                <h2> Invalid Zipcode </h2>
+                <h2> Something went wrong! </h2>
                 <h3> Temp {-10000}&#8457; </h3>
                 <h6> {props.errorMessage} </h6>
             </div>
